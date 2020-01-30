21f48aec2c9cb511af5a2d74cd9e9b315aa32e17
Jessica Heggem <jessicaheggem@gmail.com>
2020-01-30 11:20:10 -0600

    set up components and npm installed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-
-
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Search from '../Search/Search'
+import Favorites from '../Favorites/Favorites'
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Giphy Search!</h1>
-      </div>
+      <Router>
+        <Link to="/">Search</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Route exact path="/" component={Search} />
+        <Route path="/favorites" component={Favorites} />
+
+        <div>
+          <h1>Giphy Search!</h1>
+        </div>
+      </Router>
+
     );
   }
-  
+
 }
 
 export default App;
---
+++ src/components/Favorites/Favorites.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Favorites extends Component {
+
+  render() {
+    return (
+      <h1>Favorites</h1>
+
+    );
+  }
+
+}
+
+export default Favorites;
